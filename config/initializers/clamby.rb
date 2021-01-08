e14afb501912e4d52940c37814141d6ec5ed07a5
Iulia R Ionita <iulia@ionita.me>
2021-01-08 13:29:42 +0000

    Removed the extra buildpack and updated clamby config

--- config/initializers/clamby.rb
+++ config/initializers/clamby.rb
@@ -4,5 +4,6 @@ Clamby.configure({
                    :error_clamscan_missing => true,
                    :error_file_missing => true,
                    :error_file_virus => true,
-                   :output_level => 'high'
+                   :output_level => 'high',
+                   :config_file => '/app/config/antivirus/clamd.conf'
                  })
