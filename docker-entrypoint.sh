984c1f4ab349fef83ba0b05c763d63c5be09c48a
Iulia R Ionita <iulia@ionita.me>
2021-01-18 17:25:04 +0000

    Working setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 
 # because the APP_RUN_SIDEKIQ parameter is defined in /Environment/ccs/cmp and not /Environment/ccs/cmpsidekiq,
 # we have to check for it being false, so we can run the rails server, otherwise run sidekiq
-echo TCPAddr clamavd > /etc/clamav/clamd.conf && echo TCPSocket 3310 >> /etc/clamav/clamd.conf
+echo TCPAddr ccs-conclave-document-clamav.apps.internal > /etc/clamav/clamd.conf && echo TCPSocket 3310 >> /etc/clamav/clamd.conf
 
 bundle exec rails server
