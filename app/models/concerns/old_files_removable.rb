1caad8f4f1d30dfff2cac2d2c1e654b355d8e8a4
Iulia R Ionita <iulia@ionita.me>
2021-02-09 18:40:45 +0000

    Scheduler done
    Schedule fully working
    
    Added new empty line to end of files that do not have it
    
    Added new empty line to end of files that do not have it
    
    Rubocop fixes
    
    Some refactoring and added more tests

---
+++ app/models/concerns/old_files_removable.rb
@@ -0,0 +1,14 @@
+module OldFilesRemovable
+  extend ActiveSupport::Concern
+
+  included do
+    scope :older_than_3_months, -> { where('created_at >= ?', 3.months.ago.to_date) }
+
+    def self.remove_old_files
+      self.older_than_3_months.each do |document|
+        document.remove_document_file!
+        document.save
+      end
+    end
+  end
+end
