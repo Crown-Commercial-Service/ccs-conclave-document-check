52d47041e973a56678c7a7c8f9f63c0ab7685d35
Iulia R Ionita <iulia@ionita.me>
2021-08-18 15:52:13 +0100

    commented out virus check

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,7 +2,7 @@ class DocumentsController < ApplicationController
   def update
     unchecked_document = UncheckedDocument.find_by(document_id: params[:id])
     if unchecked_document&.document
-      unchecked_document.run_virus_scan_worker
+      # unchecked_document.run_virus_scan_worker
       document = unchecked_document.document.as_json.deep_transform_keys! { |key| key.camelize(:lower) }
       render json: document, status: :ok
     else
