6ac294f9412445f870b527e608c14f16053ae94f
Iulia R Ionita <iulia@ionita.me>
2021-08-18 16:35:02 +0100

    Rubocop fix

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,12 +1,12 @@
-# class DocumentsController < ApplicationController
-#   def update
-#     unchecked_document = UncheckedDocument.find_by(document_id: params[:id])
-#     if unchecked_document&.document
-#       # unchecked_document.run_virus_scan_worker
-#       document = unchecked_document.docume./spec/requests/documents_request_spec.rbnt.as_json.deep_transform_keys! { |key| key.camelize(:lower) }
-#       render json: document, status: :ok
-#     else
-#       render status: :not_found
-#     end
-#   end
-# end
+class DocumentsController < ApplicationController
+  def update
+    unchecked_document = UncheckedDocument.find_by(document_id: params[:id])
+    if unchecked_document&.document
+      # unchecked_document.run_virus_scan_worker
+      # document = unchecked_document.document.as_json.deep_transform_keys! { |key| key.camelize(:lower) }
+      render json: document, status: :ok
+    else
+      render status: :not_found
+    end
+  end
+end
