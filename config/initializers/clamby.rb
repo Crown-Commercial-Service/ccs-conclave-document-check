92f69c21ac8d1015a7ccdc9f45210a60876282e5
georgeschena <georgeschena@hotmail.com>
2023-07-03 13:42:30 +0100

    Update clambd config

--- config/initializers/clamby.rb
+++ config/initializers/clamby.rb
@@ -7,15 +7,15 @@ if Rails.env.test? || Rails.env.development?
                      error_file_virus: true,
                      output_level: 'high'
                    })
-
 else
   Clamby.configure({
                      daemonize: true,
                      stream: true,
+                     error_clamscan_client_error: true,
                      error_clamscan_missing: true,
                      error_file_missing: true,
                      error_file_virus: true,
-                     config_file: '.apt/etc/clamav/clamd.conf'
+                     config_file: '/app/config/antivirus/clamd.conf',
+                     output_level: 'high'
                    })
-
 end
