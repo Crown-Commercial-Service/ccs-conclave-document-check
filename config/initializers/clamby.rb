b26ba7c1032c6893a06e88dbf6d2c50ac05b0670
georgeschena <georgeschena@hotmail.com>
2023-07-05 15:00:20 +0100

    Lint

--- config/initializers/clamby.rb
+++ config/initializers/clamby.rb
@@ -1,32 +1,30 @@
 if Rails.env.test? || Rails.env.development?
   Clamby.configure({
-    daemonize: true,
-    stream: true,
-    error_clamscan_missing: true,
-    error_file_missing: true,
-    error_file_virus: true,
-    output_level: 'high'
-  })
+                     daemonize: true,
+                     stream: true,
+                     error_clamscan_missing: true,
+                     error_file_missing: true,
+                     error_file_virus: true,
+                     output_level: 'high'
+                   })
+elsif ENV['AWS_NATIVE'] == 'true'
+  Clamby.configure({
+                     daemonize: true,
+                     stream: true,
+                     error_clamscan_client_error: true,
+                     error_clamscan_missing: true,
+                     error_file_missing: true,
+                     error_file_virus: true,
+                     config_file: '/app/config/antivirus/clamd.conf'
+                   })
 else
-  if ENV['AWS_NATIVE'] == 'true'
-    Clamby.configure({
-      daemonize: true,
-      stream: true,
-      error_clamscan_client_error: true,
-      error_clamscan_missing: true,
-      error_file_missing: true,
-      error_file_virus: true,
-      config_file: '/app/config/antivirus/clamd.conf'
-    })
-  else
-    Clamby.configure({
-      daemonize: true,
-      stream: true,
-      error_clamscan_missing: true,
-      error_file_missing: true,
-      error_file_virus: true,
-      config_file: '.apt/etc/clamav/clamd.conf',
-      output_level: 'high'
-    })
-  end
+  Clamby.configure({
+                     daemonize: true,
+                     stream: true,
+                     error_clamscan_missing: true,
+                     error_file_missing: true,
+                     error_file_virus: true,
+                     config_file: '.apt/etc/clamav/clamd.conf',
+                     output_level: 'high'
+                   })
 end
