fdaf12663b15c55434c808e2d9d783673af33ecb
SamB-CCS <sam.boothroyd@crowncommercial.gov.uk>
2023-09-12 14:57:02 +0100

    add datadog config

--- config/initializers/datadog_tracer.rb
+++ config/initializers/datadog_tracer.rb
@@ -1,7 +1,7 @@
 # config/initializers/datadog-tracer.rb
 Datadog.configure do |c|
-    c.env = 'development'
-    c.service = 'document check'
-    c.tracing.sampling.default_rate = 1.0
-    c.appsec.enabled = true
-  end
+  c.env = 'development'
+  c.service = 'document check'
+  c.tracing.sampling.default_rate = 1.0
+  c.appsec.enabled = true
+end
