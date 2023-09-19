0737ef18e49dd848cf3dbf76cf22957ac97d551d
SamB-CCS <sam.boothroyd@crowncommercial.gov.uk>
2023-09-19 12:09:04 +0100

    amend datadog config file for trial

--- config/initializers/datadog_tracer.rb
+++ config/initializers/datadog_tracer.rb
@@ -4,4 +4,6 @@ Datadog.configure do |c|
   c.service = 'document check'
   c.tracing.sampling.default_rate = 1.0
   c.appsec.enabled = true
+  c.agent.host = '18.130.247.224'
+  c.agent.port = 8126
 end
